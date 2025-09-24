09d3625605b1b394b943487f39e78d47f5074573
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2025-09-24 16:40:10 +0530

    add patientstab

--- src/Components/PatientDashboard/PatientsTab.jsx
+++ src/Components/PatientDashboard/PatientsTab.jsx
@@ -7,19 +7,22 @@ import WeightTrend from "../PatientComponents/WeightTrend";
 import DietaryTracker from "../PatientComponents/DietaryTracker";
 import ActiveDietPlan from "../PatientComponents/ActiveDietPlan";
 import MedicalRecords from "../PatientComponents/MedicalRecords";
-import Appointments from "../PatientComponents/Appointments"; 
+import Appointments from "../PatientComponents/Appointments";
 import { patientData } from "../data/mockData";
 
 export default function PatientsTab() {
   return (
-    <div className="p-4 md:p-8 bg-gray-50 min-h-screen font-sans">
+    <div className="p-4 md:p-6 lg:p-8 bg-gray-50 min-h-screen font-sans">
       {/* Header */}
-      <DashboardHeader patient={patientData} />
+      <div className="mb-6">
+        <DashboardHeader patient={patientData} />
+      </div>
 
       {/* Grid Layout */}
-      <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4">
+        
         {/* Column 1 */}
-        <div className="grid gap-8">
+        <div className="grid gap-6">
           <PrakritiSnapshot
             prakriti={patientData.prakriti}
             vikriti={patientData.vikriti}
@@ -28,19 +31,20 @@ export default function PatientsTab() {
           <LifestyleLog log={patientData.lifestyleLog} />
         </div>
 
-        {/* Column 2 (spans 2 cols on XL screens) */}
-        <div className="grid gap-8 lg:col-span-2">
+        {/* Column 2 (main content, spans 2 cols on XL) */}
+        <div className="grid gap-6 md:col-span-2 lg:col-span-1 xl:col-span-2">
           <WeightTrend data={patientData.weightTrend} />
           <DietaryTracker analysis={patientData.dietaryAnalysis} />
           <ActiveDietPlan plan={patientData.activeDietPlan} />
         </div>
 
-        {/* Column 3 */}
-        <div className="grid gap-8 xl:col-span-1">
+        {/* Column 3 (Appointments + Medical Records) */}
+        <div className="grid gap-6 md:col-span-1 lg:col-span-1">
           <Appointments list={patientData.appointments} />
           <MedicalRecords records={patientData.medicalRecords} />
         </div>
+       
       </div>
     </div>
   );
-}
+}   

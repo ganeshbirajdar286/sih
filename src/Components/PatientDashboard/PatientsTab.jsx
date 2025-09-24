bb782c0b7e96f80dcd32827dd7abba8b3631a527
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2025-09-24 09:05:48 +0530

    responsiveness issues in patientTab.jsx

--- src/Components/PatientDashboard/PatientsTab.jsx
+++ src/Components/PatientDashboard/PatientsTab.jsx
@@ -3,12 +3,11 @@ import DashboardHeader from "../PatientComponents/DashboardHeader";
 import PrakritiSnapshot from "../PatientComponents/PrakritiSnapshot";
 import LifestyleLog from "../PatientComponents/LifestyleLog";
 import Alerts from "../PatientComponents/Alerts";
-import HerbalRecommendations from "../PatientComponents/HerbalRecommendations";
 import WeightTrend from "../PatientComponents/WeightTrend";
 import DietaryTracker from "../PatientComponents/DietaryTracker";
 import ActiveDietPlan from "../PatientComponents/ActiveDietPlan";
 import MedicalRecords from "../PatientComponents/MedicalRecords";
-import Appointments from "../PatientComponents/Appointments"; // ✅ Fixed import
+import Appointments from "../PatientComponents/Appointments"; 
 import { patientData } from "../data/mockData";
 
 export default function PatientsTab() {
@@ -18,29 +17,26 @@ export default function PatientsTab() {
       <DashboardHeader patient={patientData} />
 
       {/* Grid Layout */}
-      <div className="grid grid-cols-1 lg:col-span-3 xl:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {/* Column 1 */}
-        <div className="grid gap-8 lg:col-span-3">
+        <div className="grid gap-8">
           <PrakritiSnapshot
             prakriti={patientData.prakriti}
             vikriti={patientData.vikriti}
           />
           <Alerts alerts={patientData.alerts} />
-          <HerbalRecommendations
-            recommendations={patientData.herbalRecommendations}
-          />
+          <LifestyleLog log={patientData.lifestyleLog} />
         </div>
 
-        {/* Column 2 (spans 2 cols on large screens) */}
-        <div className="grid gap-8 lg:col-span-3">
+        {/* Column 2 (spans 2 cols on XL screens) */}
+        <div className="grid gap-8 lg:col-span-2">
           <WeightTrend data={patientData.weightTrend} />
           <DietaryTracker analysis={patientData.dietaryAnalysis} />
           <ActiveDietPlan plan={patientData.activeDietPlan} />
         </div>
 
         {/* Column 3 */}
-        <div className="grid gap-8 lg:order-first xl:order-last">
-          <LifestyleLog log={patientData.lifestyleLog} />
+        <div className="grid gap-8 xl:col-span-1">
           <Appointments list={patientData.appointments} />
           <MedicalRecords records={patientData.medicalRecords} />
         </div>

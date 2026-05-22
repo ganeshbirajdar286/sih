2a2454f3fabfa272da9a29b8a1f961f965e273fc
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2026-05-22 14:45:43 +0530

    feat: increase max request limit in rate limiting middleware

--- backend/middleware/rate.middleware.js
+++ backend/middleware/rate.middleware.js
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimitMiddleware = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 5,
+  max: 10,
 
   message: {
     success: false,

beb3b4384a142ce9a7f01969e87579439b6a781c
Ganesh <ganeshbirajdar286@gmail.com>
2026-05-26 17:41:09 +0530

    Update rate.middleware.js

--- backend/middleware/rate.middleware.js
+++ backend/middleware/rate.middleware.js
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimitMiddleware = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 10,
+  max: 20,
 
   message: {
     success: false,

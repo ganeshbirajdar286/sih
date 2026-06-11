4cd802c9dcf2be19d5ee4cf241f6e6341a98c2b8
ganeshbirajdar286 <ganeshbirajdar286@gmail.com>
2026-06-11 22:36:20 +0530

    feat: implement clustering for improved performance and add autocannon for load testing

--- backend/index.js
+++ backend/index.js
@@ -1,70 +1,87 @@
-import express from "express"
-import  dotenv from "dotenv"
-import authRouter from "./routes/auth.routes.js"
-import connect_db from "./config/db.connted.js"
+import express from "express";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.routes.js";
+import connect_db from "./config/db.connted.js";
 import cookieParser from "cookie-parser";
-import  cors from "cors"
+import cors from "cors";
 import { createServer } from "http";
 import initializeSocket from "./services/video-call-services.js";
-import {rateLimitMiddleware} from "./middleware/rate.middleware.js" 
-import helmet from "helmet"
-import "./config/Worker.config.js"
-
-dotenv.config()
-
-connect_db()
-
-const allowedOrigins = [
-  /\.vercel\.app$/,
-  ...process.env.FRONTEND_URL.split(","),
-];
-
-const corsOption = {
-  origin: function (origin, callback) {
-    // Allow mobile apps, Postman etc
-    if (!origin) return callback(null, true);
-
-    const isAllowed = allowedOrigins.some((allowed) =>
-      typeof allowed === "string"
-        ? allowed === origin
-        : allowed.test(origin)
-    );
-
-    if (isAllowed) {
-      return callback(null, true);
-    }
-    console.log("❌ CORS blocked:", origin);
-    return callback(new Error("Not allowed by CORS"));
-  },
-
-  credentials: true,
-
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-const app=express();
-const server = createServer(app);
-const io = initializeSocket(server);
-
-const port =process.env.Port || 3009
-app.use(cors(corsOption))
-
-app.use(rateLimitMiddleware);
-app.use(helmet())
-app.use(cookieParser());   
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-app.use("/api/auth",authRouter)
-
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "OK" });
-});
+import { rateLimitMiddleware } from "./middleware/rate.middleware.js";
+import helmet from "helmet";
+import "./config/Worker.config.js";
+import cluster from "node:cluster";
+import { availableParallelism } from "node:os";
 
 
-server.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on port ${port}`); 
-});
- 
+const numCPUs = availableParallelism();
+console.log(numCPUs);
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  dotenv.config();
+
+  connect_db();
+
+  const allowedOrigins = [
+    /\.vercel\.app$/,
+    ...process.env.FRONTEND_URL.split(","),
+  ];
+
+  
+
+  const corsOption = {
+    origin: function (origin, callback) {
+      // Allow mobile apps, Postman etc
+      if (!origin) return callback(null, true);
+
+      const isAllowed = allowedOrigins.some((allowed) =>
+        typeof allowed === "string" ? allowed === origin : allowed.test(origin),
+      );
+
+      if (isAllowed) {
+        return callback(null, true);
+      }
+      console.log("❌ CORS blocked:", origin);
+      return callback(new Error("Not allowed by CORS"));
+    },
+
+    credentials: true,
+
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+
+    allowedHeaders: ["Content-Type", "Authorization"],
+  };
+
+  const app = express();
+  const server = createServer(app);
+  const io = initializeSocket(server);
+
+  const port = process.env.Port || 3009;
+  app.use(cors(corsOption));
+
+  app.use(rateLimitMiddleware);
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use("/api/auth", authRouter);
+
+  app.get("/health", (req, res) => {
+    res.status(200).json({ status: "OK" });
+  });
+
+  server.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port} `);
+  });
+}
